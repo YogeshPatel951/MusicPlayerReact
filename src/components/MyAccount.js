a559e5fe9af8c1a40c08a4dccf1cf6bd745ca8bf
Yogesh Patel <patelyogesh951@gmail.com>
2020-05-29 14:25:07 +0530

    Final

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -8,24 +8,31 @@ const MyAccount = () => {
   return (
     <div>
       <Navbar />
-      <div className="roundAcc White red "><span className="mt- fsize">My Account</span></div>
+      <div className="roundAcc White red pt-1"><span className="mt- fsize">My Account</span></div>
 
       <div className="d-flex flex-column">
         <div className="mt-4 roundback White">
+        <span><i className="far fa-user mt-4 ml-3" style={{position:"absolute",zIndex:"20000",fontSize:"1.5rem",color:"red"}}></i></span>
           <div className=" fsize accrowup accrow  " >USER  PROFILE</div>
           <div className=" accrow detail">+919999999999</div>
         </div>
         <div className=" mt-4   roundback White">
+        <span><i className="fas fa-th-large mt-4 ml-3" style={{position:"absolute",zIndex:"20000",fontSize:"1.5rem",color:"red"}}></i></span>
+
           <div className=" fsize accrowup accrow " >CURRENTLY ACTIVE PLAN</div>
           <div className=" accrow detail">GAMECAST MONTHLY</div>
 
         </div>
         <div className=" mt-4 roundback White">
+        <span><i className="far fa-check-square mt-4 ml-3" style={{position:"absolute",zIndex:"20000",fontSize:"1.5rem",color:"red"}}></i></span>
+
           <div className=" fsize  accrowup accrow " >LAST CHARGED ON</div>
           <div className=" accrow detail">10 May,2020 19:10:00</div>
 
         </div>
         <div className=" mt-4 roundback White">
+        <span><i className="far fa-clock mt-4 ml-3" style={{position:"absolute",zIndex:"20000",fontSize:"1.5rem",color:"red"}}></i></span>
+
           <div className=" fsize  accrowup accrow " >NEXT BILLING DUE ON</div>
           <div className=" accrow detail">10 June, 2020</div>
         </div>
@@ -33,7 +40,7 @@ const MyAccount = () => {
 
         <button type="button" className=" btn  cancelButton White" /* onClick={gotoHome} */>
           <Link to="/" className="White ">
-            <span className="cancelWidht"> Cancel</span>
+            <span className="cancelWidht"> Cancel My Plan</span>
           </Link >
         </button>
 
