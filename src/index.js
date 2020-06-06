06847ea493177f56c36dbe1e2320300839f7b50f
Yogesh Patel <patelyogesh951@gmail.com>
2020-06-06 22:35:33 +0530

    Complete

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
-    <Route component={App} />
+    <App></App>
   </Router>,
   document.getElementById("root")
 );
